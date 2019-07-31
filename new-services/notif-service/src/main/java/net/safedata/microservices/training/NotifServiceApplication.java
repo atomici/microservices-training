5985e988abe512393ad98251080cd19b60f0f2d3
halmaged <dacian.halmagean@oce.com>
2019-07-31 14:42:21 +0300

    notif

--- new-services/notif-service/src/main/java/net/safedata/microservices/training/NotifServiceApplication.java
+++ new-services/notif-service/src/main/java/net/safedata/microservices/training/NotifServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecondServiceApplication {
+public class NotifServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecondServiceApplication.class, args);
+		SpringApplication.run(NotifServiceApplication.class, args);
 	}
 }
--- new-services/notif-service/src/test/java/net/safedata/microservices/training/NotifServiceApplicationTests.java
+++ new-services/notif-service/src/test/java/net/safedata/microservices/training/NotifServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SecondServiceApplicationTests {
+public class NotifServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
